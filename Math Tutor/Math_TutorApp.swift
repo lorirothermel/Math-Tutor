a79bbab6c6a34d39bce6c90c184ac4826a0d62a6
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-03-25 19:06:16 -0400

    Initial Commit

---
+++ Math Tutor/Math_TutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_TutorApp.swift
+//  Math Tutor
+//
+//  Created by Lori Rothermel on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Math_TutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
